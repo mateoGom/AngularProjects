5122a84ba4bd30bad1a9b88c954c5416c10ac35b
mateoGom <matemgm@hotmail.com>
2020-10-01 21:09:27 +0200

    commit 2

---
+++ project1/src/app/lista-destinos/lista-destinos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-destinos',
+  templateUrl: './lista-destinos.component.html',
+  styleUrls: ['./lista-destinos.component.css'],
+})
+export class ListaDestinosComponent implements OnInit {
+  destinos: string[];
+  constructor() {
+    this.destinos = ['barranca', 'cali', 'medallo'];
+  }
+
+  ngOnInit(): void {}
+}
