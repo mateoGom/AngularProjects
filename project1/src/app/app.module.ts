05418eae3cd937813e28e0e2e9cdb8cf729ffbdd
mateoGom <matemgm@hotmail.com>
2020-10-02 18:59:17 +0200

    commit 6

--- project1/src/app/app.module.ts
+++ project1/src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { DestinoViajeComponent } from './destino-viaje/destino-viaje.component';
 import { ListaDestinosComponent } from './lista-destinos/lista-destinos.component';
+import { DestinoDetalleComponent } from './destino-detalle/destino-detalle.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: ListaDestinosComponent },
+  { path: 'destino', component: DestinoDetalleComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    
+
     DestinoViajeComponent,
-    
-    ListaDestinosComponent
+
+    ListaDestinosComponent,
+
+    DestinoDetalleComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
